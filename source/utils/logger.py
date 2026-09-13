d612141e45f82252ce0d5eba1c59cbfd1891ff9d
BasharMithan <basharmithan@gmail.com>
2026-09-13 20:11:17 +0300

    Fix: Adding dynamic storage folder check.

--- source/utils/logger.py
+++ source/utils/logger.py
@@ -2,6 +2,10 @@ from pathlib import Path
 
 import logging as Logger
 
+# Encure that the storage folder exists
+storageFolder = Path("storage")
+storageFolder.mkdir(exist_ok=True)
+
 
 Logger.basicConfig(
     filename=str(Path(__file__).resolve().parents[2] / 'storage' / 'log.log'), 
